1e8b553912d42631a034fc1879e1da90ac5b6b9e
Sergey Zaborovsky <git@seryiza.xyz>
2026-08-11 12:02:49 +0600

    Revert "linux: downgrade to longterm version"
    This reverts commit d5d8b374839292d2549f35b88e64d3c64401f7ce.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -46,7 +46,7 @@ in {
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = unstable-pkgs.linuxPackagesFor unstable-pkgs.linux_latest;
   boot.kernelParams = [ ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "amdxdna" ];
