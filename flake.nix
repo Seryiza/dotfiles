39a58688d984d473b479c43e397ddfc163039d5d
Sergey Zaborovsky <git@seryiza.xyz>
2026-09-04 17:20:31 +0600

    chore(omp): update omp version

--- flake.nix
+++ flake.nix
@@ -27,8 +27,7 @@
     iwmenu.url = "github:e-tho/iwmenu";
     rep.url = "github:eraserhd/rep";
     llm-agents.url = "github:numtide/llm-agents.nix";
-    # TODO: Remove the commit pin after https://github.com/can1357/oh-my-pi/pull/10281 is merged.
-    omp.url = "github:can1357/oh-my-pi/42c80b2d1e4f86a4f302f7835fec0687158759a5";
+    omp.url = "github:can1357/oh-my-pi";
     sysc-greet = {
       url = "github:Nomadcxx/sysc-greet";
       inputs.nixpkgs.follows = "nixpkgs";
