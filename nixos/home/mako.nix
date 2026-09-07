461f590de1ff7e4ffe7a78befc55a7276663ae5d
Sergey Zaborovsky <git@seryiza.xyz>
2026-09-07 17:03:31 +0600

    feat(mako): play notification sounds

--- nixos/home/mako.nix
+++ nixos/home/mako.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 let
   volumeSound = "${pkgs.yaru-theme}/share/sounds/Yaru/stereo/audio-volume-change.oga";
+  notificationSound = "${pkgs.yaru-theme}/share/sounds/Yaru/stereo/message.oga";
+  playSound = file:
+    "exec ${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play --cache-control=volatile --file=${file}";
   patchedMako = pkgs.mako.overrideAttrs (oldAttrs: {
     # Remove when https://github.com/emersion/mako/issues/636 is fixed upstream.
     patches = (oldAttrs.patches or [ ]) ++ [
@@ -18,14 +21,14 @@ in
       "text-color" = "#ffffff";
       "border-size" = 0;
       layer = "overlay";
+      "on-notify" = playSound notificationSound;
 
       "app-name=ya-vol" = {
         history = 0;
         anchor = "top-center";
         group-by = "app-name";
         format = "<b>%s</b>%b";
-        "on-notify" =
-          "exec ${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play --cache-control=volatile --file=${volumeSound}";
+        "on-notify" = playSound volumeSound;
       };
 
       "app-name=ya-backlight" = {
@@ -33,6 +36,7 @@ in
         anchor = "top-center";
         group-by = "app-name";
         format = "<b>%s</b>%b";
+        "on-notify" = playSound volumeSound;
       };
     };
   };
